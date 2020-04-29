5ebcedb95895336377896a238d5c78998bb543cb
Ross Fairgrieve <ross.fairgrieve1@gmail.com>
2020-04-29 17:12:59 +0100

    Added integration of Goodreads data on individual book pages

--- application.py
+++ application.py
@@ -4,6 +4,8 @@ from flask import Flask, session, render_template, request, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -34,7 +36,8 @@ def index():
 @app.route("/search", methods=["GET", "POST"])
 def search():
     if request.method == "GET":
-        return render_template("search.html", show_logout=1)
+        return render_template("search.html",
+                               showlogout=1)
     else:
         search_type = request.form.get("search_by")
         search_term = request.form.get("search_term").lower()
@@ -42,7 +45,10 @@ def search():
             search_term = f"%{search_term}%"
         results = db.execute(f"SELECT * FROM books WHERE lower({search_type}) LIKE (:search_term);",
                              {"search_term": search_term}).fetchall()
-        return render_template("search-results.html", results=results, num_results=len(results), show_logout=1)
+        return render_template("search-results.html",
+                               results=results,
+                               num_results=len(results),
+                               showlogout=1)
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -57,7 +63,8 @@ def register():
             return render_template("register.html", message="Username already taken")
         results = db.execute("SELECT * FROM users WHERE email = :email", {"email": email}).fetchone()
         if results != None:
-            return render_template("register.html", message="An account with that email address already exists")
+            return render_template("register.html",
+                                   message="An account with that email address already exists")
         else:
             password = request.form.get("password")
             db.execute("""INSERT INTO users (username, password, email)
@@ -73,14 +80,18 @@ def login():
         username = request.form.get("username")
         results = db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).fetchone()
         if results == None:
-            return render_template("login.html", message="Username does not exist.")
+            return render_template("login.html",
+                                   message="Username does not exist.")
         else:
             password = request.form.get("password")
             if password == results.password:
                 session["username"] = username
-                return render_template("login-success.html", username=username, show_logout=1)
+                return render_template("login-success.html",
+                                       username=username,
+                                       showlogout=1)
             else:
-                return render_template("login.html", message="Password incorrect.")
+                return render_template("login.html",
+                                       message="Password incorrect.")
     else:
         return render_template("login.html")
 
@@ -88,22 +99,58 @@ def login():
 def book(isbn):
     book = db.execute("SELECT * FROM books WHERE isbn = :isbn;", {"isbn": isbn}).fetchone()
     reviews = db.execute("SELECT * FROM reviews WHERE isbn = :isbn;", {"isbn": isbn}).fetchall()
+
+    # Get information from Goodreads API
+    # If ISBN is in Goodreads database, store the ratings_count and average_rating values
+    try:
+        goodreads = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "0KCqHvur2Et2F6blBKLDTA", "isbns": isbn}).json()
+        grcount = goodreads["books"][0]["ratings_count"]
+        grrating = goodreads["books"][0]["average_rating"]
+
+    # If ISBN is not in Goodreads database, set values to None to allow for
+    # conditional rendering in book.html
+    except:
+        grcount = None
+        grrating = None
+
+    # Render the following if accessing via link from search results (GET request)
     if request.method == "GET":
+
         # List details of a single book
-        return render_template("book.html", book=book, reviews=reviews, errormessage="", successmessage="", show_logout=1)
+        return render_template("book.html",
+                               book=book,
+                               grcount=grcount,
+                               grrating=grrating,
+                               reviews=reviews,
+                               errormessage="",
+                               successmessage="",
+                               showlogout=1)
+
+    # Do this if accessing via the review submission form (POST request)
     else:
-        # Get details from review form and attempt to post review
+        book = db.execute("SELECT * FROM books WHERE isbn = :isbn;", {"isbn": isbn}).fetchone()
+
+        # Get details from review form
         username = session.get("username")
         isbn = request.form.get("isbn")
         rating = request.form.get("rating")
         revtext = request.form.get("revtext")
-        book = db.execute("SELECT * FROM books WHERE isbn = :isbn;", {"isbn": isbn}).fetchone()
+
         # Check to see if the reviewer has already posted a review of this book
         old_reviews = db.execute("SELECT * FROM reviews WHERE username = :username AND isbn = :isbn",
                              {"username": username, "isbn": isbn}).fetchone()
+
         # If user has already reviewed book, return to book page with alert
         if old_reviews != None:
-            return render_template("book.html", book=book, reviews=reviews, errormessage="You have already reviewed this book", successmessage="", show_logout=1)
+            return render_template("book.html",
+                                   book=book,
+                                   grcount=grcount,
+                                   grrating=grrating,
+                                   reviews=reviews,
+                                   errormessage="You have already reviewed this book",
+                                   successmessage="",
+                                   showlogout=1)
+
         # If user hasn't already reviewed book, write review to database
         else:
             db.execute("""INSERT INTO reviews (username, isbn, rating, revtext)
@@ -111,8 +158,14 @@ def book(isbn):
                        {"username": username, "isbn": isbn, "rating": rating, "revtext": revtext})
             db.commit()
             reviews = db.execute("SELECT * FROM reviews WHERE isbn = :isbn;", {"isbn": isbn}).fetchall()
-            return render_template("book.html", book=book, reviews=reviews, errormessage="", successmessage="Your review has been posted", show_logout=1)
+            return render_template("book.html",
+                                   book=book,
+                                   reviews=reviews,
+                                   errormessage="",
+                                   successmessage="Your review has been posted",
+                                   showlogout=1)
 
+# Allow logged in user to logout by clearing the session
 @app.route("/logout")
 def logout():
     session.clear()
